1367ab5613f0ac86f3e2823556873efaf6e7bd9b
saewoo1 <wooozooo1123@icloud.com>
2024-01-21 18:01:48 +0900

    FEAT: 닉네임 중복 기능

--- src/main/java/deepdive/backend/member/controller/MemberController.java
+++ src/main/java/deepdive/backend/member/controller/MemberController.java
@@ -5,6 +5,11 @@ import deepdive.backend.member.service.MemberService;
 import deepdive.backend.utils.response.Response;
 import deepdive.backend.utils.response.ResponseMsg;
 import deepdive.backend.utils.response.StatusCode;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,12 +19,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/member")
+@RequestMapping("/api/v1/member")
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
-
 
     /**
      * 마케팅 동의를 마친 회원을 등록합니다.
@@ -27,8 +31,15 @@ public class MemberController {
      * @param dto 유저에 대한 기본 정보 - 이메일, 발급자, 동의내역, 접속 장소
      * @return 성공 시 200, 실패 시 401
      */
+    @Operation(summary = "회원 가입 신청", description = "isResgistered = false 유저들을 신규 등록합니다")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "신규 유저 등록"),
+        @ApiResponse(responseCode = "400", description = "가입한 내역이 존재합니다. 다른 email로 시도해주세요.")
+    })
     @PostMapping("/register")
-    public ResponseEntity<Response> registerUser(@RequestBody RegisterMemberDto dto) {
+    public ResponseEntity<Response> registerUser(
+        @Parameter(description = "회원가입 진행 시 필요한 기본 정보")
+        @RequestBody @Valid RegisterMemberDto dto) {
         memberService.registerMember(dto);
 
         return new ResponseEntity<>(Response.of(StatusCode.OK, ResponseMsg.UNREGISTERED),
--- src/main/java/deepdive/backend/member/domain/dto/RegisterMemberDto.java
+++ src/main/java/deepdive/backend/member/domain/dto/RegisterMemberDto.java
@@ -1,15 +1,23 @@
 package deepdive.backend.member.domain.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@Schema(description = "회원가입 진행 시 유저의 기본 정보")
 public class RegisterMemberDto {
 
+    @NotNull
     private String email;
+    @NotNull
     private String provider;
+    @NotNull
     private Boolean isAlarm;
+    @NotNull
     private Boolean isMarketing;
+    //    @NotNull
     private String locale;
 }
