8a3c990f4ed72045f57f9ef084e094a5d1c35afc
saewoo1 <wooozooo1123@icloud.com>
2024-02-07 20:25:58 +0900

    FIX: 경로 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ROOT_PATH="/home/ubuntu/deepdive-github-action"
+SOURCE_JAR="$ROOT_PATH/build/libs/backend-0.0.1-SNAPSHOT.jar"
 JAR="$ROOT_PATH/application.jar"
 
 APP_LOG="$ROOT_PATH/application.log"
@@ -9,8 +10,11 @@ START_LOG="$ROOT_PATH/start.log"
 
 NOW=$(date +%c)
 
+echo "[$NOW] 현재 디렉토리: $(pwd)" >> "$START_LOG"
+echo "[$NOW] build/libs 디렉토리 내용: $(ls -la "$ROOT_PATH/build/libs")" >> "$START_LOG"
+
 echo "[$NOW] $JAR 복사" >> $START_LOG
-cp "$ROOT_PATH/build/libs/deepdive-github-action/backend-0.0.1-SNAPSHOT.jar" "$JAR"
+cp "$SOURCE_JAR" "$JAR" >> "$START_LOG"
 
 echo "[$NOW] $JAR 실행" >> $START_LOG
 nohup java -jar "$JAR" > "$APP_LOG" 2> "$ERROR_LOG" &
