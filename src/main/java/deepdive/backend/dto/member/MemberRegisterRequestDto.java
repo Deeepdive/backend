5813853c7a93a00df355be907811e32fb57cb0f3
saewoo1 <wooozooo1123@icloud.com>
2024-02-15 14:19:46 +0900

    FEAT: 이메일 검증 기능 추가

--- src/main/java/deepdive/backend/dto/member/MemberRegisterRequestDto.java
+++ src/main/java/deepdive/backend/dto/member/MemberRegisterRequestDto.java
@@ -1,6 +1,13 @@
 package deepdive.backend.dto.member;
 
-public record MemberRegisterRequestDto(String email, String provider, Boolean isAlarm,
-                                       Boolean isMarketing) {
+import jakarta.validation.constraints.Email;
+
+public record MemberRegisterRequestDto(
+    @Email(message = "이메일 형식으로 입력해주세요.")
+    String email,
+    String provider,
+    Boolean isAlarm,
+    Boolean isMarketing
+) {
 
 }
