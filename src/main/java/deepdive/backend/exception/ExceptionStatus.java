799cf4c9d96d93fcf4da5d7816290972810bc2e4
saewoo1 <wooozooo1123@icloud.com>
2024-02-17 00:36:47 +0900

    FEAT: EnumValidation 어노테이션 추가

--- src/main/java/deepdive/backend/exception/ExceptionStatus.java
+++ src/main/java/deepdive/backend/exception/ExceptionStatus.java
@@ -15,6 +15,9 @@ public enum ExceptionStatus {
     NOT_FOUND_LOG(HttpStatus.NOT_FOUND, "존재하지 않는 로그입니다."),
     DUPLICATE_REGISTER(HttpStatus.BAD_REQUEST, "가입한 내역이 존재합니다. 다른 email로 시도해주세요."),
     DUPLICATE_NICKNAME(HttpStatus.CONFLICT, "해당 닉네임은 이미 사용중입니다."),
+    DUPLICATE_GOOGLE(HttpStatus.CONFLICT, "google sns로 가입한 이력이 있습니다."),
+    DUPLICATE_NAVER(HttpStatus.CONFLICT, "naver sns로 가입한 이력이 있습니다."),
+    DUPLICATE_KAKAO(HttpStatus.CONFLICT, "kakao sns로 가입한 이력이 있습니다."),
     INVALID_ARGUMENT(HttpStatus.BAD_REQUEST, "유효하지 않은 입력입니다"),
     INVALID_USER_ID(HttpStatus.BAD_REQUEST, "허용되지 않은 ID 값입니다."),
     INVALID_REGISTER(HttpStatus.BAD_REQUEST, "SNS 정보와 회원 정보가 일치하지 않습니다."),
@@ -22,7 +25,9 @@ public enum ExceptionStatus {
     INVALID_CERT_TYPE(HttpStatus.BAD_REQUEST, "알 수 없는 자격증 명입니다."),
     INVALID_MATCH_PROFILE(HttpStatus.BAD_REQUEST, "발급 기관과 자격증 유형이 일치하지 않습니다."),
     INVALID_NUMBER_TYPE(HttpStatus.BAD_REQUEST, "번호와 일치하는 url이 존재하지 않습니다."),
-    NOT_FOUND_PROFILE(HttpStatus.NOT_FOUND, "프로필이 존재하지 않습니다.");
+    NOT_FOUND_PROFILE(HttpStatus.NOT_FOUND, "프로필이 존재하지 않습니다."),
+    INVALID_BUDDY_PROFILE(HttpStatus.BAD_REQUEST, "스스로 buddy로 추가할 수 없습니다"),
+    INVALID_ENUM_TYPE(HttpStatus.BAD_REQUEST, "Enum 타입이 일치하지 않습니다.");
 
     private final int errorCode;
     private final String message;
