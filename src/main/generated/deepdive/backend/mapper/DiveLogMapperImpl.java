1b6e16ae72b4c6dfded1126c702696430f566374
saewoo1 <wooozooo1123@icloud.com>
2024-02-17 12:08:10 +0900

    FEAT: login attribute log

--- src/main/generated/deepdive/backend/mapper/DiveLogMapperImpl.java
+++ src/main/generated/deepdive/backend/mapper/DiveLogMapperImpl.java
@@ -1,20 +1,13 @@
 package deepdive.backend.mapper;
 
-import deepdive.backend.divelog.domain.AirTankInformation;
-import deepdive.backend.divelog.domain.DiveHistory;
-import deepdive.backend.divelog.domain.DiveInformation;
 import deepdive.backend.divelog.domain.Purpose;
-import deepdive.backend.divelog.domain.Review;
-import deepdive.backend.divelog.domain.SuitType;
-import deepdive.backend.divelog.domain.UnderWaterVisibility;
-import deepdive.backend.divelog.domain.WaterType;
-import deepdive.backend.divelog.domain.Weather;
-import deepdive.backend.divelog.domain.WeightType;
 import deepdive.backend.divelog.domain.entity.DiveLog;
 import deepdive.backend.dto.divelog.DiveLogInfoDto;
 import deepdive.backend.dto.divelog.DiveLogResponseDto;
 import deepdive.backend.dto.divelog.DiveLogResponsePaginationDto;
 import deepdive.backend.dto.profile.ProfileDefaultDto;
+import deepdive.backend.dto.profile.ProfileDefaultResponseDto;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.processing.Generated;
@@ -22,54 +15,101 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-02-09T15:07:32+0900",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.7 (Oracle Corporation)"
+    date = "2024-02-17T11:25:54+0900",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.7 (Oracle Corporation)"
 )
 @Component
 public class DiveLogMapperImpl implements DiveLogMapper {
 
     @Override
-    public DiveLogInfoDto toDiveLogInfoDto(DiveLog diveLog) {
-
-        if ( diveLog != null ) {
-        }
+    public DiveLogInfoDto toDiveLogInfoDto(DiveLog diveLog, List<String> buddyNames) {
 
         Long id = null;
-        Review review = null;
-        DiveHistory diveHistory = null;
-        AirTankInformation airTankInformation = null;
-        DiveInformation diveInformation = null;
+        String purpose = null;
+        LocalDate diveDate = null;
+        String center = null;
+        String point = null;
+        String waterType = null;
+        Long depth = null;
+        Long diveMin = null;
+        Long waterTemp = null;
+        String underWaterVisibility = null;
         Long airTemp = null;
+        String weather = null;
+        String suitType = null;
         Long weight = null;
-        Purpose purpose = null;
-        WaterType waterType = null;
-        UnderWaterVisibility underWaterVisibility = null;
-        Weather weather = null;
-        SuitType suitType = null;
-        WeightType weightType = null;
-
-        DiveLogInfoDto diveLogInfoDto = new DiveLogInfoDto( id, review, diveHistory, airTankInformation, diveInformation, airTemp, weight, purpose, waterType, underWaterVisibility, weather, suitType, weightType );
-
+        String weightType = null;
+        Long startPressure = null;
+        Long endPressure = null;
+        Long airTankUsage = null;
+        String reviewType = null;
+        String reviewComment = null;
         if ( diveLog != null ) {
+            id = diveLog.getId();
+            if ( diveLog.getPurpose() != null ) {
+                purpose = diveLog.getPurpose().name();
+            }
+            diveDate = diveLog.getDiveDate();
+            center = diveLog.getCenter();
+            point = diveLog.getPoint();
+            if ( diveLog.getWaterType() != null ) {
+                waterType = diveLog.getWaterType().name();
+            }
+            depth = diveLog.getDepth();
+            diveMin = diveLog.getDiveMin();
+            waterTemp = diveLog.getWaterTemp();
+            if ( diveLog.getUnderWaterVisibility() != null ) {
+                underWaterVisibility = diveLog.getUnderWaterVisibility().name();
+            }
+            airTemp = diveLog.getAirTemp();
+            if ( diveLog.getWeather() != null ) {
+                weather = diveLog.getWeather().name();
+            }
+            if ( diveLog.getSuitType() != null ) {
+                suitType = diveLog.getSuitType().name();
+            }
+            weight = diveLog.getWeight();
+            if ( diveLog.getWeightType() != null ) {
+                weightType = diveLog.getWeightType().name();
+            }
+            startPressure = diveLog.getStartPressure();
+            endPressure = diveLog.getEndPressure();
+            airTankUsage = diveLog.getAirTankUsage();
+            if ( diveLog.getReviewType() != null ) {
+                reviewType = diveLog.getReviewType().name();
+            }
+            reviewComment = diveLog.getReviewComment();
         }
+        List<String> buddyNames1 = null;
+        List<String> list = buddyNames;
+        if ( list != null ) {
+            buddyNames1 = new ArrayList<String>( list );
+        }
+
+        DiveLogInfoDto diveLogInfoDto = new DiveLogInfoDto( id, purpose, diveDate, center, point, waterType, depth, diveMin, waterTemp, underWaterVisibility, airTemp, weather, suitType, weight, weightType, startPressure, endPressure, airTankUsage, reviewType, reviewComment, buddyNames1 );
 
         return diveLogInfoDto;
     }
 
     @Override
-    public DiveLogResponseDto toDiveLogResponseDto(DiveLog diveLog, List<ProfileDefaultDto> buddyProfiles) {
-
-        List<ProfileDefaultDto> buddyProfiles1 = null;
-        List<ProfileDefaultDto> list = buddyProfiles;
-        if ( list != null ) {
-            buddyProfiles1 = new ArrayList<ProfileDefaultDto>( list );
-        }
+    public DiveLogResponseDto toDiveLogResponseDto(DiveLog diveLog, List<ProfileDefaultResponseDto> buddyProfiles) {
 
         Long id = null;
-        DiveHistory diveHistory = null;
+        LocalDate diveDate = null;
+        String center = null;
+        String point = null;
         Purpose purpose = null;
+        if ( diveLog != null ) {
+            id = diveLog.getId();
+            diveDate = diveLog.getDiveDate();
+            center = diveLog.getCenter();
+            point = diveLog.getPoint();
+            purpose = diveLog.getPurpose();
+        }
+        List<ProfileDefaultDto> buddyProfiles1 = null;
+        buddyProfiles1 = profileDefaultResponseDtoListToProfileDefaultDtoList( buddyProfiles );
 
-        DiveLogResponseDto diveLogResponseDto = new DiveLogResponseDto( id, diveHistory, purpose, buddyProfiles1 );
+        DiveLogResponseDto diveLogResponseDto = new DiveLogResponseDto( id, diveDate, center, point, purpose, buddyProfiles1 );
 
         return diveLogResponseDto;
     }
@@ -88,5 +128,35 @@ public class DiveLogMapperImpl implements DiveLogMapper {
         DiveLogResponsePaginationDto diveLogResponsePaginationDto = new DiveLogResponsePaginationDto( result1, totalCount1 );
 
         return diveLogResponsePaginationDto;
+    }
+
+    protected ProfileDefaultDto profileDefaultResponseDtoToProfileDefaultDto(ProfileDefaultResponseDto profileDefaultResponseDto) {
+
+        String nickName = null;
+        String picture = null;
+        if ( profileDefaultResponseDto != null ) {
+            nickName = profileDefaultResponseDto.nickName();
+            picture = profileDefaultResponseDto.picture();
+        }
+
+        ProfileDefaultDto profileDefaultDto = new ProfileDefaultDto( nickName, picture );
+
+        if ( profileDefaultResponseDto != null ) {
+        }
+
+        return profileDefaultDto;
+    }
+
+    protected List<ProfileDefaultDto> profileDefaultResponseDtoListToProfileDefaultDtoList(List<ProfileDefaultResponseDto> list) {
+        if ( list == null ) {
+            return new ArrayList<ProfileDefaultDto>();
+        }
+
+        List<ProfileDefaultDto> list1 = new ArrayList<ProfileDefaultDto>( list.size() );
+        for ( ProfileDefaultResponseDto profileDefaultResponseDto : list ) {
+            list1.add( profileDefaultResponseDtoToProfileDefaultDto( profileDefaultResponseDto ) );
+        }
+
+        return list1;
     }
 }
