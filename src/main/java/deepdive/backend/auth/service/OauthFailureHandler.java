1f8cc3c85f9b8993d584466a4cf9d1adece19c2b
saewoo1 <wooozooo1123@icloud.com>
2024-02-17 12:11:31 +0900

    FEAT: login attribute log

--- src/main/java/deepdive/backend/auth/service/OauthFailureHandler.java
+++ src/main/java/deepdive/backend/auth/service/OauthFailureHandler.java
@@ -4,16 +4,20 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class OauthFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
         AuthenticationException exception) throws IOException, ServletException {
-        response.sendRedirect("http://localhost:3000/loginFail");
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        response.getWriter().write("login Fail, check your server log");
+        log.info("로그인 실패 = {}", exception.getMessage());
     }
 }
