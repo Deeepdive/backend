5c7f829e79bbdb709cf175bd60f7606310a15c91
saewoo1 <wooozooo1123@icloud.com>
2024-02-18 23:43:26 +0900

    FEAT: 기본 이미지 url 추가

--- src/main/java/deepdive/backend/profile/domain/Pictures.java
+++ src/main/java/deepdive/backend/profile/domain/Pictures.java
@@ -5,12 +5,18 @@ import java.util.Arrays;
 import lombok.Getter;
 
 public enum Pictures {
-    FIRST(1, "testurl1"),
-    SECOND(2, "testurl2"),
-    THIRD(3, "testurl3"),
-    FOURTH(4, "testurl4"),
-    FIFTH(5, "testurl5"),
-    SIXTH(6, "testurl6");
+    FIRST(1,
+        "https://github.com/Deeepdive/backend/assets/92019454/a71b756d-5cd7-43c9-8980-693be851099a"),
+    SECOND(2,
+        "https://github.com/Deeepdive/backend/assets/92019454/e4d9e3bf-acc5-4d01-9dba-2fa68d4812c2"),
+    THIRD(3,
+        "https://github.com/Deeepdive/backend/assets/92019454/f764648d-5559-4bdb-99ec-73d7add5f2cd"),
+    FOURTH(4,
+        "https://github.com/Deeepdive/backend/assets/92019454/80106dbb-0f90-496a-b5b2-6fba7b1fadc1"),
+    FIFTH(5,
+        "https://github.com/Deeepdive/backend/assets/92019454/47a41bfd-dac3-4ee5-a631-487fc1142e99"),
+    SIXTH(6,
+        "https://github.com/Deeepdive/backend/assets/92019454/692fae28-6d71-4a94-a185-ba42de9a352f");
 
     private final int number;
     @Getter
