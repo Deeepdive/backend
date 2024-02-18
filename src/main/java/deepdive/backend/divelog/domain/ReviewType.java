5d466655cd3aeea8c5ae0c99d3cb35df83b38012
saewoo1 <wooozooo1123@icloud.com>
2024-02-18 14:38:02 +0900

    FIX: enum 이름 변경

--- src/main/java/deepdive/backend/divelog/domain/ReviewType.java
+++ src/main/java/deepdive/backend/divelog/domain/ReviewType.java
@@ -2,5 +2,5 @@ package deepdive.backend.divelog.domain;
 
 public enum ReviewType {
 
-    VERY_GOOD, GOOD, AVERAGE, POOR
+    VERY_GOOD, GOOD, NORMAL, POOR
 }
--- src/main/java/deepdive/backend/divelog/domain/UnderWaterVisibility.java
+++ src/main/java/deepdive/backend/divelog/domain/UnderWaterVisibility.java
@@ -2,6 +2,6 @@ package deepdive.backend.divelog.domain;
 
 public enum UnderWaterVisibility {
     GOOD,
-    AVERAGE,
+    NORMAL,
     POOR
 }
