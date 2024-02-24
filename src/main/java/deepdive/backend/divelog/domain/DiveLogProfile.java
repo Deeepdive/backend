96bf170901e494b7d6dd01fbbfadd34841b00b1b
saewoo1 <wooozooo1123@icloud.com>
2024-02-24 13:22:46 +0900

    REFACTOR: defaultImage 반환 시 object 객체로 포장 후 반환

---
+++ src/main/java/deepdive/backend/divelog/domain/DiveLogProfile.java
@@ -0,0 +1,26 @@
+package deepdive.backend.divelog.domain;
+
+import deepdive.backend.divelog.domain.entity.DiveLog;
+import deepdive.backend.profile.domain.entity.Profile;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class DiveLogProfile {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "dive_log_id")
+	private DiveLog diveLog;
+
+	@ManyToOne
+	@JoinColumn(name = "profile_id")
+	private Profile profile;
+}
--- src/main/java/deepdive/backend/divelog/domain/entity/DiveLog.java
+++ src/main/java/deepdive/backend/divelog/domain/entity/DiveLog.java
@@ -30,122 +30,113 @@ import lombok.Setter;
 @Getter
 public class DiveLog {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "divelog_id")
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "divelog_id")
+	private Long id;
 
-    @Setter
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
-    private Member member;
+	@Setter
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
 
-    @Setter
-    @OneToMany(targetEntity = Profile.class, fetch = FetchType.LAZY)
-    private List<Profile> profiles;
+	@Setter
+	@OneToMany(targetEntity = Profile.class, fetch = FetchType.LAZY)
+	private List<Profile> profiles;
 
-//    @Embedded
-//    private Review review;
-//    @Embedded
-//    private DiveHistory diveHistory;
-//    @Embedded
-//    private AirTankInformation airTankInformation;
-//    @Embedded
-//    private DiveInformation diveInformation;
+	// TODO : 나중에 객체화 하기..
+	@Enumerated(value = EnumType.STRING)
+	private ReviewType reviewType;
+	private String reviewComment;
 
-    // TODO : 나중에 객체화 하기..
-    @Enumerated(value = EnumType.STRING)
-    private ReviewType reviewType;
-    private String reviewComment;
-
-    private LocalDate diveDate;
-    private String center;
-    private String point;
+	private LocalDate diveDate;
+	private String center;
+	private String point;
 
 
-    private Long startPressure;
-    private Long endPressure;
-    private Long airTankUsage;
+	private Long startPressure;
+	private Long endPressure;
+	private Long airTankUsage;
 
-    private Long depth;
-    private Long diveMin;
-    private Long waterTemp;
-
-
-    private Long airTemp;
-    private Long weight;
+	private Long depth;
+	private Long diveMin;
+	private Long waterTemp;
 
 
-    @Enumerated(value = EnumType.STRING)
-    private Purpose purpose;
-    @Enumerated(value = EnumType.STRING)
-    private WaterType waterType;
-    @Enumerated(value = EnumType.STRING)
-    private UnderWaterVisibility underWaterVisibility;
-    @Enumerated(value = EnumType.STRING)
-    private Weather weather;
-    @Enumerated(value = EnumType.STRING)
-    private SuitType suitType;
-    @Enumerated(value = EnumType.STRING)
-    private WeightType weightType;
+	private Long airTemp;
+	private Long weight;
 
-    protected DiveLog() {
 
-    }
+	@Enumerated(value = EnumType.STRING)
+	private Purpose purpose;
+	@Enumerated(value = EnumType.STRING)
+	private WaterType waterType;
+	@Enumerated(value = EnumType.STRING)
+	private UnderWaterVisibility underWaterVisibility;
+	@Enumerated(value = EnumType.STRING)
+	private Weather weather;
+	@Enumerated(value = EnumType.STRING)
+	private SuitType suitType;
+	@Enumerated(value = EnumType.STRING)
+	private WeightType weightType;
 
-    public static DiveLog of(DiveLogRequestDto dto, List<Profile> profiles) {
-        DiveLog diveLog = new DiveLog();
-        diveLog.purpose = Purpose.valueOf(dto.purpose());
-        diveLog.underWaterVisibility = UnderWaterVisibility.valueOf(dto.underWaterVisibility());
-        diveLog.weather = Weather.valueOf(dto.weather());
-        diveLog.suitType = SuitType.valueOf(dto.suitType());
-        diveLog.weightType = WeightType.valueOf(dto.weightType());
-        diveLog.reviewType = ReviewType.valueOf(dto.reviewType());
-        diveLog.waterType = WaterType.valueOf(dto.waterType());
+	protected DiveLog() {
 
-        // 소분류별 객체 분리
-        diveLog.reviewComment = dto.reviewComment();
-        diveLog.diveDate = dto.diveDate();
-        diveLog.center = dto.center();
-        diveLog.point = dto.point();
-        diveLog.startPressure = dto.startPressure();
-        diveLog.endPressure = dto.endPressure();
-        diveLog.airTankUsage = dto.airTankUsage();
-        diveLog.depth = dto.depth();
-        diveLog.diveMin = dto.diveMin();
-        diveLog.waterTemp = dto.waterTemp();
+	}
 
-        diveLog.airTemp = dto.airTemp();
-        diveLog.weight = dto.weight();
+	public static DiveLog of(DiveLogRequestDto dto, List<Profile> profiles) {
+		DiveLog diveLog = new DiveLog();
+		diveLog.purpose = Purpose.valueOf(dto.purpose());
+		diveLog.underWaterVisibility = UnderWaterVisibility.valueOf(dto.underWaterVisibility());
+		diveLog.weather = Weather.valueOf(dto.weather());
+		diveLog.suitType = SuitType.valueOf(dto.suitType());
+		diveLog.weightType = WeightType.valueOf(dto.weightType());
+		diveLog.reviewType = ReviewType.valueOf(dto.reviewType());
+		diveLog.waterType = WaterType.valueOf(dto.waterType());
 
-        diveLog.profiles = profiles;
+		// 소분류별 객체 분리
+		diveLog.reviewComment = dto.reviewComment();
+		diveLog.diveDate = dto.diveDate();
+		diveLog.center = dto.center();
+		diveLog.point = dto.point();
+		diveLog.startPressure = dto.startPressure();
+		diveLog.endPressure = dto.endPressure();
+		diveLog.airTankUsage = dto.airTankUsage();
+		diveLog.depth = dto.depth();
+		diveLog.diveMin = dto.diveMin();
+		diveLog.waterTemp = dto.waterTemp();
 
-        return diveLog;
-    }
+		diveLog.airTemp = dto.airTemp();
+		diveLog.weight = dto.weight();
 
-    public void update(DiveLogRequestDto dto, List<Profile> buddies) {
-        // enum 타입
-        this.waterType = WaterType.valueOf(dto.waterType());
-        this.reviewType = ReviewType.valueOf(dto.reviewType());
-        this.purpose = Purpose.valueOf(dto.purpose());
-        this.suitType = SuitType.valueOf(dto.suitType());
-        this.weather = Weather.valueOf(dto.weather());
-        this.weightType = WeightType.valueOf(dto.weightType());
-        this.underWaterVisibility = UnderWaterVisibility.valueOf(dto.underWaterVisibility());
+		diveLog.profiles = profiles;
 
-        this.reviewComment = dto.reviewComment();
-        this.diveDate = dto.diveDate();
-        this.center = dto.center();
-        this.point = dto.point();
-        this.profiles = buddies;
-        this.startPressure = dto.startPressure();
-        this.endPressure = dto.endPressure();
-        this.airTankUsage = dto.airTankUsage();
-        this.depth = dto.depth();
-        this.diveMin = dto.diveMin();
-        this.waterTemp = dto.waterTemp();
+		return diveLog;
+	}
 
-        this.airTemp = dto.airTemp();
-        this.weight = dto.weight();
-    }
+	public void update(DiveLogRequestDto dto, List<Profile> buddies) {
+		// enum 타입
+		this.waterType = WaterType.valueOf(dto.waterType());
+		this.reviewType = ReviewType.valueOf(dto.reviewType());
+		this.purpose = Purpose.valueOf(dto.purpose());
+		this.suitType = SuitType.valueOf(dto.suitType());
+		this.weather = Weather.valueOf(dto.weather());
+		this.weightType = WeightType.valueOf(dto.weightType());
+		this.underWaterVisibility = UnderWaterVisibility.valueOf(dto.underWaterVisibility());
+
+		this.reviewComment = dto.reviewComment();
+		this.diveDate = dto.diveDate();
+		this.center = dto.center();
+		this.point = dto.point();
+		this.profiles = buddies;
+		this.startPressure = dto.startPressure();
+		this.endPressure = dto.endPressure();
+		this.airTankUsage = dto.airTankUsage();
+		this.depth = dto.depth();
+		this.diveMin = dto.diveMin();
+		this.waterTemp = dto.waterTemp();
+
+		this.airTemp = dto.airTemp();
+		this.weight = dto.weight();
+	}
 }
