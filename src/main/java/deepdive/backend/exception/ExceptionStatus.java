d4e41f97d16a54602b17518eaa6eadad5ae652e0
saewoo1 <wooozooo1123@icloud.com>
2024-02-25 18:17:19 +0900

    FIX: 금칙어 포함 시 반환하는 에러 메세지 변경
    타입 에러와 구분지어서 에러메세지를 반환

--- src/main/java/deepdive/backend/exception/ExceptionStatus.java
+++ src/main/java/deepdive/backend/exception/ExceptionStatus.java
@@ -10,46 +10,47 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 @Getter
 public enum ExceptionStatus {
-    NOT_FOUND_USER(HttpStatus.NOT_FOUND, "존재하지 않는 유저입니다."),
-    NOT_FOUND_USER_BY_EMAIL(HttpStatus.NOT_FOUND, "이메일과 일치하는 유저가 존재하지 않습니다."),
-    NOT_FOUND_LOG(HttpStatus.NOT_FOUND, "존재하지 않는 로그입니다."),
-    DUPLICATE_REGISTER(HttpStatus.BAD_REQUEST, "가입한 내역이 존재합니다. 다른 sns로 시도해주세요."),
-    DUPLICATE_NICKNAME(HttpStatus.CONFLICT, "해당 닉네임은 이미 사용중입니다."),
-    DUPLICATE_GOOGLE(HttpStatus.CONFLICT, "google sns로 가입한 이력이 있습니다."),
-    DUPLICATE_NAVER(HttpStatus.CONFLICT, "naver sns로 가입한 이력이 있습니다."),
-    DUPLICATE_KAKAO(HttpStatus.CONFLICT, "kakao sns로 가입한 이력이 있습니다."),
-    INVALID_ARGUMENT(HttpStatus.BAD_REQUEST, "유효하지 않은 입력입니다"),
-    INVALID_USER_ID(HttpStatus.BAD_REQUEST, "허용되지 않은 ID 값입니다."),
-    INVALID_PROVIDER(HttpStatus.BAD_REQUEST, "허용되지 않은 provider 입니다."),
-    INVALID_REGISTER(HttpStatus.BAD_REQUEST, "SNS 정보와 회원 정보가 일치하지 않습니다."),
-    INVALID_ORGANIZATION_TYPE(HttpStatus.BAD_REQUEST, "알 수 없는 기관입니다."),
-    INVALID_CERT_TYPE(HttpStatus.BAD_REQUEST, "알 수 없는 자격증 명입니다."),
-    INVALID_MATCH_PROFILE(HttpStatus.BAD_REQUEST, "발급 기관과 자격증 유형이 일치하지 않습니다."),
-    INVALID_NUMBER_TYPE(HttpStatus.BAD_REQUEST, "번호와 일치하는 url이 존재하지 않습니다."),
-    DUPLICATE_APPLE(HttpStatus.CONFLICT, "apple sns로 가입한 이력이 있습니다."),
-    INVALID_DIVE_DATE(HttpStatus.BAD_REQUEST, "현재 날짜(한국 기준)보다 이틀 이후의 date는 입력할 수 없습니다."),
-    NOT_FOUND_PROFILE(HttpStatus.NOT_FOUND, "프로필이 존재하지 않습니다."),
-    INVALID_NICKNAME(HttpStatus.NOT_FOUND, "닉네임은 영문, 숫자, 길이는 20자 이하여야합니다."),
-    INVALID_OS(HttpStatus.NOT_FOUND, "IOS, ANDROID 형식이 올바르지 않습니다."),
-    INVALID_REGISTER_TOKEN(HttpStatus.UNAUTHORIZED, "login 시 요청한 oauthId가 일치하지 않습니다."),
-    INVALID_BUDDY_PROFILE(HttpStatus.BAD_REQUEST, "스스로 buddy로 추가할 수 없습니다"),
-    INVALID_ENUM_TYPE(HttpStatus.BAD_REQUEST, "Enum 타입이 일치하지 않습니다.");
+	NOT_FOUND_USER(HttpStatus.NOT_FOUND, "존재하지 않는 유저입니다."),
+	NOT_FOUND_USER_BY_EMAIL(HttpStatus.NOT_FOUND, "이메일과 일치하는 유저가 존재하지 않습니다."),
+	NOT_FOUND_LOG(HttpStatus.NOT_FOUND, "존재하지 않는 로그입니다."),
+	DUPLICATE_REGISTER(HttpStatus.BAD_REQUEST, "가입한 내역이 존재합니다. 다른 sns로 시도해주세요."),
+	DUPLICATE_NICKNAME(HttpStatus.CONFLICT, "해당 닉네임은 이미 사용중입니다."),
+	DUPLICATE_GOOGLE(HttpStatus.CONFLICT, "google sns로 가입한 이력이 있습니다."),
+	DUPLICATE_NAVER(HttpStatus.CONFLICT, "naver sns로 가입한 이력이 있습니다."),
+	DUPLICATE_KAKAO(HttpStatus.CONFLICT, "kakao sns로 가입한 이력이 있습니다."),
+	INVALID_ARGUMENT(HttpStatus.BAD_REQUEST, "유효하지 않은 입력입니다"),
+	INVALID_USER_ID(HttpStatus.BAD_REQUEST, "허용되지 않은 ID 값입니다."),
+	INVALID_PROVIDER(HttpStatus.BAD_REQUEST, "허용되지 않은 provider 입니다."),
+	INVALID_REGISTER(HttpStatus.BAD_REQUEST, "SNS 정보와 회원 정보가 일치하지 않습니다."),
+	INVALID_ORGANIZATION_TYPE(HttpStatus.BAD_REQUEST, "알 수 없는 기관입니다."),
+	INVALID_CERT_TYPE(HttpStatus.BAD_REQUEST, "알 수 없는 자격증 명입니다."),
+	INVALID_MATCH_PROFILE(HttpStatus.BAD_REQUEST, "발급 기관과 자격증 유형이 일치하지 않습니다."),
+	INVALID_NUMBER_TYPE(HttpStatus.BAD_REQUEST, "번호와 일치하는 url이 존재하지 않습니다."),
+	DUPLICATE_APPLE(HttpStatus.CONFLICT, "apple sns로 가입한 이력이 있습니다."),
+	INVALID_DIVE_DATE(HttpStatus.BAD_REQUEST, "현재 날짜(한국 기준)보다 이틀 이후의 date는 입력할 수 없습니다."),
+	NOT_FOUND_PROFILE(HttpStatus.NOT_FOUND, "프로필이 존재하지 않습니다."),
+	INVALID_NICKNAME_TYPE(HttpStatus.NOT_FOUND, "닉네임은 4 ~ 20자 사이의 영문, 숫자로 이루어져야합니다."),
+	INVALID_WORD_CONTAIN(HttpStatus.NOT_FOUND, "금칙어가 포함되어 있습니다."),
+	INVALID_OS(HttpStatus.NOT_FOUND, "IOS, ANDROID 형식이 올바르지 않습니다."),
+	INVALID_REGISTER_TOKEN(HttpStatus.UNAUTHORIZED, "login 시 요청한 oauthId가 일치하지 않습니다."),
+	INVALID_BUDDY_PROFILE(HttpStatus.BAD_REQUEST, "스스로 buddy로 추가할 수 없습니다"),
+	INVALID_ENUM_TYPE(HttpStatus.BAD_REQUEST, "Enum 타입이 일치하지 않습니다.");
 
-    private final int errorCode;
-    private final String message;
-    private final String error;
+	private final int errorCode;
+	private final String message;
+	private final String error;
 
-    ExceptionStatus(HttpStatus status, String message) {
-        this.errorCode = status.value();
-        this.message = message;
-        this.error = status.getReasonPhrase();
-    }
+	ExceptionStatus(HttpStatus status, String message) {
+		this.errorCode = status.value();
+		this.message = message;
+		this.error = status.getReasonPhrase();
+	}
 
-    public ServiceException asServiceException() {
-        return new ServiceException(this);
-    }
+	public ServiceException asServiceException() {
+		return new ServiceException(this);
+	}
 
-    public DomainException asDomainException() {
-        return new DomainException(this);
-    }
+	public DomainException asDomainException() {
+		return new DomainException(this);
+	}
 }
