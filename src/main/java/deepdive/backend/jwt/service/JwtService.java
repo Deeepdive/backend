7ac31e1bf04e875b10c3f5582475982610018007
saewoo1 <wooozooo1123@icloud.com>
2024-02-25 21:39:29 +0900

    FIX: 토큰 길이 수정

--- src/main/java/deepdive/backend/jwt/service/JwtService.java
+++ src/main/java/deepdive/backend/jwt/service/JwtService.java
@@ -31,124 +31,124 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class JwtService {
 
-    private final MemberQueryService memberQueryService;
-    @Value("${jwt.token.secret}")
-    private String secret_code;
-    @Value("${refreshtoken.expires}")
-    private Long refreshTokenExpiration;
-    @Value("${accesstoken.expires}")
-    private Long accessTokenExpiration;
-    @Value("${issuer}")
-    private String issuer;
+	private final MemberQueryService memberQueryService;
+	@Value("${jwt.token.secret}")
+	private String secret_code;
+	@Value("${refreshtoken.expires}")
+	private Long refreshTokenExpiration;
+	@Value("${accesstoken.expires}")
+	private Long accessTokenExpiration;
+	@Value("${issuer}")
+	private String issuer;
 
-    public TokenInfo generateToken(Long memberId, String oauthId) {
-        return new TokenInfo(createAccessToken(memberId, oauthId), createRefreshToken(oauthId));
-    }
+	public TokenInfo generateToken(Long memberId, String oauthId) {
+		return new TokenInfo(createAccessToken(memberId, oauthId), createRefreshToken(oauthId));
+	}
 
-    public String createRefreshToken(String oauthId) {
-        Claims claims = generateClaimFormat();
+	public String createRefreshToken(String oauthId) {
+		Claims claims = generateClaimFormat();
 
-        claims.put("oauth", oauthId);
-        Date now = new Date();
+		claims.put("oauth", oauthId);
+		Date now = new Date();
 
-        return Jwts.builder()
-            .setClaims(claims)
-            .setExpiration(new Date(now.getTime() + 600 * refreshTokenExpiration))
-            .signWith(generateSecretKey(secret_code), SignatureAlgorithm.HS256)
-            .compact();
-    }
+		return Jwts.builder()
+			.setClaims(claims)
+			.setExpiration(new Date(now.getTime() + 60 * 1000 * refreshTokenExpiration))
+			.signWith(generateSecretKey(secret_code), SignatureAlgorithm.HS256)
+			.compact();
+	}
 
-    public String createAccessToken(Long memberId, String oauthId) {
-        Claims claims = generateClaimFormat();
-        claims.put("memberId", memberId);
-        claims.put("oauthId", oauthId);
-        Date now = new Date();
+	public String createAccessToken(Long memberId, String oauthId) {
+		Claims claims = generateClaimFormat();
+		claims.put("memberId", memberId);
+		claims.put("oauthId", oauthId);
+		Date now = new Date();
 
-        return Jwts.builder()
-            .setClaims(claims)
-            .setIssuer(issuer)
-            .setIssuedAt(now)
-            .setExpiration(new Date(now.getTime() + 600 * accessTokenExpiration))
-            .signWith(generateSecretKey(secret_code), SignatureAlgorithm.HS256)
-            .compact();
-    }
+		return Jwts.builder()
+			.setClaims(claims)
+			.setIssuer(issuer)
+			.setIssuedAt(now)
+			.setExpiration(new Date(now.getTime() + 60 * 1000 * accessTokenExpiration))
+			.signWith(generateSecretKey(secret_code), SignatureAlgorithm.HS256)
+			.compact();
+	}
 
-    private Claims generateClaimFormat() {
-        Claims claims = Jwts.claims().setSubject("UserToken");
-        claims.put("roles", "User");
-        return claims;
-    }
+	private Claims generateClaimFormat() {
+		Claims claims = Jwts.claims().setSubject("UserToken");
+		claims.put("roles", "User");
+		return claims;
+	}
 
-    public String createRegisterToken(String oauthId) {
-        Claims claims = generateClaimFormat();
-        claims.put("oauthId", oauthId);
-        Date now = new Date();
+	public String createRegisterToken(String oauthId) {
+		Claims claims = generateClaimFormat();
+		claims.put("oauthId", oauthId);
+		Date now = new Date();
 
-        return Jwts.builder()
-            .setClaims(claims)
-            .setIssuer(issuer)
-            .setIssuedAt(now)
-            .setExpiration(new Date(now.getTime() + 60 * 1000 * 60)) // 60 * 1000 -> 1분으로 변환
-            .signWith(generateSecretKey(secret_code), SignatureAlgorithm.HS256)
-            .compact();
-    }
+		return Jwts.builder()
+			.setClaims(claims)
+			.setIssuer(issuer)
+			.setIssuedAt(now)
+			.setExpiration(new Date(now.getTime() + 60 * 1000 * 60)) // 60 * 1000 -> 1분으로 변환
+			.signWith(generateSecretKey(secret_code), SignatureAlgorithm.HS256)
+			.compact();
+	}
 
-    public Authentication getAuthentication(String accessToken) {
-        Claims claims = parseToken(accessToken);
-        log.info("token info: {}", claims);
+	public Authentication getAuthentication(String accessToken) {
+		Claims claims = parseToken(accessToken);
+		log.info("token info: {}", claims);
 
-        if (claims.get("roles") == null) {
-            throw new IllegalArgumentException("권한 정보가 존재하지 않습니다.");
-        }
+		if (claims.get("roles") == null) {
+			throw new IllegalArgumentException("권한 정보가 존재하지 않습니다.");
+		}
 
-        List<SimpleGrantedAuthority> authorities = Stream.of(claims.get("roles").toString())
-            .map(SimpleGrantedAuthority::new)
-            .toList();
+		List<SimpleGrantedAuthority> authorities = Stream.of(claims.get("roles").toString())
+			.map(SimpleGrantedAuthority::new)
+			.toList();
 
-        Long memberId = claims.get("memberId", Long.class);
-        String oauthId = claims.get("oauthId", String.class);
+		Long memberId = claims.get("memberId", Long.class);
+		String oauthId = claims.get("oauthId", String.class);
 
-        AuthUserInfo authUserInfo = AuthUserInfo.builder()
-            .memberId(memberId)
-            .oauthId(oauthId)
-            .build();
+		AuthUserInfo authUserInfo = AuthUserInfo.builder()
+			.memberId(memberId)
+			.oauthId(oauthId)
+			.build();
 
-        log.info("authUerInfo 생성");
-        return new UsernamePasswordAuthenticationToken(authUserInfo, "", authorities);
-    }
+		log.info("authUerInfo 생성");
+		return new UsernamePasswordAuthenticationToken(authUserInfo, "", authorities);
+	}
 
-    public String reissueAccessToken(TokenInfo reIssueDto) {
+	public String reissueAccessToken(TokenInfo reIssueDto) {
 
-        // refresh encrypt 후에 괜찮은 애라면 재발급 ㄱㄱ
-        Claims claims = parseToken(reIssueDto.refreshToken());
-        String oauthId = claims.get("oauthId", String.class);
-        Member member = memberQueryService.getByOauthId(oauthId);
+		// refresh encrypt 후에 괜찮은 애라면 재발급 ㄱㄱ
+		Claims claims = parseToken(reIssueDto.refreshToken());
+		String oauthId = claims.get("oauthId", String.class);
+		Member member = memberQueryService.getByOauthId(oauthId);
 
-        return createAccessToken(member.getId(), member.getOauthId());
-    }
+		return createAccessToken(member.getId(), member.getOauthId());
+	}
 
-    private Claims parseToken(String token) {
-        try {
-            return Jwts.parserBuilder()
-                .setSigningKey(generateSecretKey(secret_code))
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
-        } catch (MalformedJwtException e) {
-            throw new JwtException("잘못된 형식의 토큰입니다");
-        } catch (ExpiredJwtException e) {
-            throw new JwtException("만료된 토큰입니다.");
-        } catch (UnsupportedJwtException e) {
-            throw new JwtException("지원하지 않는 형식의 토큰입니다");
-        } catch (IllegalArgumentException e) {
-            throw new JwtException("잘못된 정보입니다.");
-        } catch (SignatureException e) {
-            throw new JwtException("시그니쳐가 이상한데요");
-        }
-    }
+	private Claims parseToken(String token) {
+		try {
+			return Jwts.parserBuilder()
+				.setSigningKey(generateSecretKey(secret_code))
+				.build()
+				.parseClaimsJws(token)
+				.getBody();
+		} catch (MalformedJwtException e) {
+			throw new JwtException("잘못된 형식의 토큰입니다");
+		} catch (ExpiredJwtException e) {
+			throw new JwtException("만료된 토큰입니다.");
+		} catch (UnsupportedJwtException e) {
+			throw new JwtException("지원하지 않는 형식의 토큰입니다");
+		} catch (IllegalArgumentException e) {
+			throw new JwtException("잘못된 정보입니다.");
+		} catch (SignatureException e) {
+			throw new JwtException("시그니쳐가 이상한데요");
+		}
+	}
 
-    private Key generateSecretKey(String secretCode) {
-        String encodeSecretCode = Base64.getEncoder().encodeToString(secretCode.getBytes());
-        return Keys.hmacShaKeyFor(encodeSecretCode.getBytes());
-    }
+	private Key generateSecretKey(String secretCode) {
+		String encodeSecretCode = Base64.getEncoder().encodeToString(secretCode.getBytes());
+		return Keys.hmacShaKeyFor(encodeSecretCode.getBytes());
+	}
 }
