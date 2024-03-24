5118a440a6d9c5725a78c0629621941ebec00336
saewoo1 <wooozooo1123@icloud.com>
2024-03-24 19:30:20 +0900

    FEAT: csv 파일에 대한 DTO 작성

---
+++ src/main/java/deepdive/backend/dto/diveshop/DiveShopCsvData.java
@@ -0,0 +1,33 @@
+package deepdive.backend.dto.diveshop;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class DiveShopCsvData {
+
+	private String name;
+	private String phoneNumber;
+	private String fax;
+	private String fullAddress;
+	private String province;
+	private String city;
+	private String detail;
+	private String availableTime;
+	private String review;
+	private String comment;
+	private String sportType;
+
+
+	public static List<String> getFieldNames() {
+		Field[] declaredFields = DiveShopCsvData.class.getDeclaredFields();
+		List<String> result = new ArrayList<>();
+		for (Field declaredField : declaredFields) {
+			result.add(declaredField.getName());
+		}
+
+		return result;
+	}
+}
