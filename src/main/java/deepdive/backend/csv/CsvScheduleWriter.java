8550a855e757670ceabee11a7bcb6bbe95e6be84
saewoo1 <wooozooo1123@icloud.com>
2024-05-02 03:43:04 +0900

    REFACTOR: 데이터를 읽어들인 후 객체 생성 시 로직 분리

--- src/main/java/deepdive/backend/csv/CsvScheduleWriter.java
+++ src/main/java/deepdive/backend/csv/CsvScheduleWriter.java
@@ -3,14 +3,17 @@ package deepdive.backend.csv;
 import deepdive.backend.diveshop.domain.Address;
 import deepdive.backend.diveshop.domain.ContactInformation;
 import deepdive.backend.diveshop.domain.DiveShop;
+import deepdive.backend.diveshop.domain.DiveShopPicture;
 import deepdive.backend.diveshop.domain.DiveShopSport;
 import deepdive.backend.diveshop.domain.Location;
 import deepdive.backend.diveshop.domain.Sport;
+import deepdive.backend.diveshop.repository.DiveShopPictureRepository;
 import deepdive.backend.diveshop.repository.DiveShopRepository;
 import deepdive.backend.diveshop.repository.DiveShopSportRepository;
 import deepdive.backend.diveshop.repository.SportRepository;
 import deepdive.backend.dto.diveshop.DiveShopCsvData;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.item.Chunk;
@@ -28,36 +31,61 @@ public class CsvScheduleWriter implements ItemWriter<DiveShopCsvData> {
 	private final DiveShopRepository diveShopRepository;
 	private final SportRepository sportRepository;
 	private final DiveShopSportRepository diveShopSportRepository;
+	private final DiveShopPictureRepository diveShopPictureRepository;
 
 	@Override
 	@Transactional
 	public void write(Chunk<? extends DiveShopCsvData> chunk) {
 		Chunk<DiveShop> diveShops = new Chunk<>();
 		List<DiveShopSport> diveShopInformations = new ArrayList<>();
+		List<DiveShopPicture> diveShopPictures = new ArrayList<>();
 
 		chunk.forEach(diveShopCsvData -> {
-			String fullAddress = diveShopCsvData.getFullAddress();
-			Location location = naverPlaceSearchService.getDiveShopImages(fullAddress);
-			Address address = Address.of(diveShopCsvData.getProvince(), diveShopCsvData.getCity(),
-				diveShopCsvData.getFullAddress(), diveShopCsvData.getDetail());
-			ContactInformation contactInformation =
-				ContactInformation.of(diveShopCsvData.getPhoneNumber(), diveShopCsvData.getFax());
-			DiveShop diveShop = DiveShop.of(diveShopCsvData.getName(), address, contactInformation,
-				diveShopCsvData.getComment(), diveShopCsvData.getAvailableTime(), location);
+			DiveShop diveShop = createDiveShopByCsvData(diveShopCsvData);
 			diveShops.add(diveShop);
 
-			String sportTypes = diveShopCsvData.getSportType();
-			List<String> types = List.of(sportTypes.split(","));
-			for (String type : types) {
-				Sport sport = sportRepository.findByName(type)
-					.orElseGet(() -> {
-						Sport newSport = Sport.of(type);
-						return sportRepository.save(newSport);
-					});
-				diveShopInformations.add(DiveShopSport.of(diveShop, sport));
-			}
+			List<DiveShopSport> diveShopSports = createDiveShopSportByCsvData(diveShop,
+				diveShopCsvData.getSportType());
+			diveShopInformations.addAll(diveShopSports);
+
+			DiveShopPicture diveShopPicture = DiveShopPicture.of(diveShop,
+				diveShopCsvData.getThumbnail());
+			diveShopPictures.add(diveShopPicture);
+
+			List<DiveShopPicture> pictures = createDiveShopPictures(diveShop,
+				diveShopCsvData.getImages());
+			diveShopPictures.addAll(pictures);
 		});
 		diveShopRepository.saveAll(diveShops);
 		diveShopSportRepository.saveAll(diveShopInformations);
+		diveShopPictureRepository.saveAll(diveShopPictures);
+	}
+
+	private List<DiveShopSport> createDiveShopSportByCsvData(DiveShop diveShop, String sportType) {
+		return Arrays.stream(sportType.split(","))
+			.map(name -> sportRepository.findByName(name).orElseGet(() -> {
+				Sport sport = Sport.of(name);
+				return sportRepository.save(sport);
+			}))
+			.map(sport -> DiveShopSport.of(diveShop, sport))
+			.toList();
+	}
+
+	private DiveShop createDiveShopByCsvData(DiveShopCsvData diveShopCsvData) {
+		String fullAddress = diveShopCsvData.getFullAddress();
+		Location location = naverPlaceSearchService.getDiveShopImages(fullAddress);
+		Address address = Address.of(diveShopCsvData.getProvince(), diveShopCsvData.getCity(),
+			diveShopCsvData.getFullAddress(), diveShopCsvData.getDetail());
+		ContactInformation contactInformation =
+			ContactInformation.of(diveShopCsvData.getPhoneNumber(), diveShopCsvData.getFax());
+		return DiveShop.of(diveShopCsvData.getName(), address, contactInformation,
+			diveShopCsvData.getReview_comment(), diveShopCsvData.getAvailableTime(), location,
+			diveShopCsvData.getReview_title());
+	}
+
+	private List<DiveShopPicture> createDiveShopPictures(DiveShop diveShop, String images) {
+		return Arrays.stream(images.split(","))
+			.map(picture -> DiveShopPicture.of(diveShop, picture))
+			.toList();
 	}
 }
