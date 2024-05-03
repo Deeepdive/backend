977d7695a84b089bfe9f74485e5e61b5125a409e
saewoo1 <wooozooo1123@icloud.com>
2024-05-03 15:39:47 +0900

    FEAT: Spring Batch 작동 멈추기

--- src/main/java/deepdive/backend/csv/SimpleJobConfiguration.java
+++ src/main/java/deepdive/backend/csv/SimpleJobConfiguration.java
@@ -1,43 +1,43 @@
-package deepdive.backend.csv;
-
-import deepdive.backend.dto.diveshop.DiveShopCsvData;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.job.builder.JobBuilder;
-import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.PlatformTransactionManager;
-
-@Slf4j
-@Configuration
-@RequiredArgsConstructor
-public class SimpleJobConfiguration {
-
-	private final CsvReader csvReader;
-	private final CsvScheduleWriter csvScheduleWriter;
-//	private final CsvFileDeleteListener csvFileDeleteListener;
-
-	@Bean
-	public Job diveShopDataLoadJob(JobRepository jobRepository, Step diveShopDataLoadStep) {
-		return new JobBuilder("simpleJob", jobRepository)
-			.start(diveShopDataLoadStep)
-			.build();
-	}
-
-	@Bean
-	public Step diveShopDataLoadStep(
-		JobRepository jobRepository,
-		PlatformTransactionManager platformTransactionManager) {
-		return new StepBuilder("diveShopDataLoadStep", jobRepository)
-			.<DiveShopCsvData, DiveShopCsvData>chunk(50, platformTransactionManager)
-			.reader(csvReader.csvScheduleReader())
-			.writer(csvScheduleWriter)
-//			.listener(csvFileDeleteListener)
-			.build();
-	}
-
-}
+//package deepdive.backend.csv;
+//
+//import deepdive.backend.dto.diveshop.DiveShopCsvData;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.batch.core.Job;
+//import org.springframework.batch.core.Step;
+//import org.springframework.batch.core.job.builder.JobBuilder;
+//import org.springframework.batch.core.repository.JobRepository;
+//import org.springframework.batch.core.step.builder.StepBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.transaction.PlatformTransactionManager;
+//
+//@Slf4j
+//@Configuration
+//@RequiredArgsConstructor
+//public class SimpleJobConfiguration {
+//
+//	private final CsvReader csvReader;
+//	private final CsvScheduleWriter csvScheduleWriter;
+////	private final CsvFileDeleteListener csvFileDeleteListener;
+//
+//	@Bean
+//	public Job diveShopDataLoadJob(JobRepository jobRepository, Step diveShopDataLoadStep) {
+//		return new JobBuilder("simpleJob", jobRepository)
+//			.start(diveShopDataLoadStep)
+//			.build();
+//	}
+//
+//	@Bean
+//	public Step diveShopDataLoadStep(
+//		JobRepository jobRepository,
+//		PlatformTransactionManager platformTransactionManager) {
+//		return new StepBuilder("diveShopDataLoadStep", jobRepository)
+//			.<DiveShopCsvData, DiveShopCsvData>chunk(50, platformTransactionManager)
+//			.reader(csvReader.csvScheduleReader())
+//			.writer(csvScheduleWriter)
+////			.listener(csvFileDeleteListener)
+//			.build();
+//	}
+//
+//}
