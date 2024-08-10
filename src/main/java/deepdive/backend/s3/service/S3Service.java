9e7e8b8c82dbb4fe78cf66deea56a4ba461663a1
saewoo1 <wooozooo1123@icloud.com>
2024-08-10 18:02:05 +0900

    FEAT: S3에 파일 업로드 및 삭제 요청 기능

--- src/main/java/deepdive/backend/s3/service/S3Service.java
+++ src/main/java/deepdive/backend/s3/service/S3Service.java
@@ -1,14 +1,11 @@
 package deepdive.backend.s3.service;
 
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import deepdive.backend.exception.ExceptionStatus;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,14 +14,15 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
-public class S3FileUploadService {
+@Slf4j
+public class S3Service {
 
 	private final AmazonS3 s3Client;
 	private final S3PolicyService s3PolicyService;
-	@Value("{cloud.aws.s3.bucket}")
+	@Value("${cloud.aws.s3.bucket}")
 	private String bucketName;
 
-	public String upload(MultipartFile imageFile) {
+	public String uploadImage(MultipartFile imageFile) throws IOException {
 		s3PolicyService.validateImageFile(imageFile);
 		String originalFileName = imageFile.getOriginalFilename();
 
@@ -32,19 +30,21 @@ public class S3FileUploadService {
 
 		ObjectMetadata objectMetadata = new ObjectMetadata();
 		objectMetadata.setContentType(imageFile.getContentType());
-		try {
-			InputStream inputStream = imageFile.getInputStream();
-			s3Client.putObject(
-					new PutObjectRequest(bucketName, originalFileName, inputStream,
-							objectMetadata).withCannedAcl(CannedAccessControlList.PublicRead));
-		} catch (IOException e) {
-			throw ExceptionStatus.INVALID_IMAGE_CONTENT.asServiceException();
-		}
+		objectMetadata.setContentLength(imageFile.getSize());
+		s3Client.putObject(bucketName, s3FileName, imageFile.getInputStream(),
+				objectMetadata);
 
 		return s3Client.getUrl(bucketName, s3FileName).toString();
 	}
 
 	private String generateS3FileName(String fileName) {
-		return UUID.randomUUID() + fileName;
+		String generatedName = UUID.randomUUID() + fileName;
+
+		return generatedName.substring(0, 10);
+	}
+
+	// 이미지 올린 후 취소 혹은 지우기 요청
+	public void deleteImage(String fileName) {
+		s3Client.deleteObject(bucketName, fileName);
 	}
 }
